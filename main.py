e2326122910eb8df3d2c96d8a17ce63c558478d0
mafernandez9 <mafernandez9@uc.cl>
2022-08-12 00:19:10 -0400

    adding first function

---
+++ main.py
@@ -0,0 +1,18 @@
+﻿import json
+
+def most_retweeted(info):
+    return list[info.items[:10]]
+
+def most_tweets(info):
+    pairs = {}
+    for key in info:
+        if 
+
+def main(option):
+    with open('farmers-protest-tweets-2021-03-5.json') as file:
+        if option == '1':
+            data = json.load(file)
+            data_sorted = sorted(data.items, key=lambda item: item[10])
+            most_retweeted(data_sorted)
+        else if option == '2':
+
